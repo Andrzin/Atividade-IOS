6df2f2cdb78f34e42f76756ac576cb5ecbb3ddb9
Andrzin <69975897+Andrzin@users.noreply.github.com>
2021-03-01 12:28:07 -0300

    Update and rename README.md to atividade.swift

---
+++ atividade.swift
@@ -0,0 +1,25 @@
+
+//Atividade 1 
+var algumacoisa int = 555
+var outracoisa = "dedezin o brabo"
+var maisUmaCoisa = 3.14159
+
+//Atividade 2
+let ano int = 2013
+let krmorados double = 63751
+var preco ="R$15.000"
+let fabricante : String = "Fiat"
+
+//Atividade 3
+print("uma constante do exemplo: let constantName = <initial value> nunca vai mudar")
+ print("ja uma variável do tipo var constantName:<data type> pode sempre receber novos valores")
+
+ //Atividade 4
+var tipoint: Int = 55
+var tipodou: Double = 5.55
+var juntos : String = "println(tipoint) + println(tipodou)"
+
+//Atividade 5
+print("Type Safety seria uma forma segura de declarar variaveis por exemplo: (var welcomeMessage: String)")
+print("Ja no Type Inference se você não especificar o tipo de valor de que precisa, o Swift usa a inferência de tipo para descobrir o tipo apropriado. A inferência de tipo permite que um compilador deduza o tipo de uma expressão específica automaticamente ao compilar seu código , simplesmente examinando os valores fornecidos.")
+
